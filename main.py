97ef5df575481687d8554ba2d60d90cd6adbdefb
Shpriz <shpriz1982@gmail.com>
2024-12-28 10:45:40 +0300

    Add simple dockerfile

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from app import create_app
-from waitress import serve
 
 app = create_app()
 
@@ -7,7 +6,7 @@ app = create_app()
 if __name__ == '__main__':
     port: int = app.config.get('FLASK_RUN_PORT', 5000)
     debug = app.config.get('FLASK_DEBUG', False)
-    serve(app, port=port)
+    app.run(debug=debug, port=port)
 
 
 @app.errorhandler(413)
